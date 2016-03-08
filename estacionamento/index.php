38de388f9782ee465b382c9420e9ed276bbc1eb7
Marco Augusto <marcoaugustosantinho@gmail.com>
2016-03-07 22:03:15 -0300

    inclusao de estilo na pagina inicial

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Pagina inicial do projeto</title>
+	<link rel="stylesheet" href="bootstrap/min.css"/>
 </head>
 <body>
 	<h1>Página inicial do projeto</h1>
+	<?php print "Impresso em PHP";?>
 </body>
 </html>
