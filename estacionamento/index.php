4414c7ebb6605fbffd265db7b8a6817b56de12b0
Marco Augusto <marcoaugustosantinho@gmail.com>
2016-03-07 21:54:20 -0300

    Estruturação da página inicial do projeto

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,2 +1,10 @@
-<?php
-print "pagina inicial do projeto";
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Pagina inicial do projeto</title>
+</head>
+<body>
+	<h1>Página inicial do projeto</h1>
+</body>
+</html>
